51ea9042a4397734b073ef4e8c38388789539bbc
sx-SDG <93168063+sx-SDG@users.noreply.github.com>
2024-01-01 10:51:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ main.py -r        #弱口令
 main.py xxx.txt   #字典爆破
 默认使用的代理:{proxies}
 ''')
-# url="https://agent.dgs-zopqucm.com/agent/#/"
 url="https://127.0.0.1/user/login"
 def userpassbp(result,file,ints): #爆破
     headersbp = {
